868346d1a1bb3c0e59f3f8c5f7264c60e091f3aa
Xienda-d <2374211067@qq.com>
2026-09-22 17:01:41 +0800

    添加pom和单元测试

---
+++ src/test/java/TestSubtraction.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSubtraction {
+
+    @Test
+    void subtraction(){
+        int actualResult = 20 - 10;
+        assertEquals(10, actualResult);
+    }
+}
